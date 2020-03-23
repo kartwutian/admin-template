e7ee7a43fd23221a42a2fbf730b28fbd147d1b04
baohz <baohz@zjbdos.com>
2020-03-23 10:24:17 +0800

    地图update

--- src/layout/Root/index.js
+++ src/layout/Root/index.js
@@ -5,14 +5,14 @@ import { hot } from 'react-hot-loader';
 import { Router, Route, Redirect, Switch } from 'react-router-dom';
 import { createHashHistory } from 'history';
 import { Provider } from 'mobx-react';
-import App from 'layout/App';
+// import App from 'layout/App';
 import store from 'store';
 import { ConfigProvider } from 'antd';
-import loginUtil from 'util/login';
+// import loginUtil from 'util/login';
 import zh_CN from 'antd/es/locale/zh_CN';
 import 'moment/locale/zh-cn';
 
-import Home from 'pages/Home';
+// import Home from 'pages/Home';
 
 import 'stylesheet/cantd.less';
 import 'stylesheet/app.less';
